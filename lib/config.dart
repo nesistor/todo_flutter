d61ad487f84711ab34b4e728c522c35bd49fa1a9
Karol Malicki <goappdm@gmail.com>
2023-07-31 23:21:35 +0200

    created Config

--- lib/config.dart
+++ lib/config.dart
@@ -1,5 +1,17 @@
 class Config {
   static String get apiUrl {
-    return 'https://backend.todo.app/api';
+    return 'https://127.0.0.1/api';
+  }
+  static String get apiCreateTask {
+    return "$apiUrl/tasks-service/create_task";
+  }
+  static String apiDeleteTask(id) {
+    return "$apiUrl/tasks-service/delete_task/$id";
+  }
+  static String apiUpdateTask(id) {
+    return "$apiUrl/tasks-service/udpate_task/$id";
+  }
+  static String get apiGetTasks {
+    return "$apiUrl/tasks-service/get_tasks";
   }
 }
